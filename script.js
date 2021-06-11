b3a67d36d620f787fc557b6871a64566d8bda1a4
kumar889 <kumar.bajura@gmail.com>
2021-06-11 14:19:24 +0545

    Assignment4,commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var number = 120; //integer
+var stringOne = "5150"; //string
+var stringTwo= "TWO HUNDRED THIRTY" //string
+
+//print in same line:
+console.log (number,stringOne,stringTwo);
+
+//print in different line:
+console.log (number);
+console.log (stringOne);
+console.log (stringTwo);
+
+//print  in same statement but different line:
+console.log(number + '\n' + stringOne + '\n' + stringTwo);
+
+//print converting string into number:
+console.log(stringOne);
